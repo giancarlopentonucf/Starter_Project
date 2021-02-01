8ad414b72f7f7b3604b36caf414b733245f2bcd2
giancarlopentonucf <70389731+giancarlopentonucf@users.noreply.github.com>
2021-02-01 15:24:12 -0500

    Audio Update

---
+++ Starter_Project/Assets/Scripts/DelayedPlay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayedPlay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource backgroundMusic = gameObject.GetComponent<AudioSource>();
+        backgroundMusic.PlayDelayed(3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
