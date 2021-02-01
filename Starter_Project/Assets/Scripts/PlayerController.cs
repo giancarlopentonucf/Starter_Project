7575b6bb71c27daec0e7a6af7acea6d5b6e7e684
giancarlopentonucf <70389731+giancarlopentonucf@users.noreply.github.com>
2021-02-01 18:51:56 -0500

    Count Update

--- Starter_Project/Assets/Scripts/PlayerController.cs
+++ Starter_Project/Assets/Scripts/PlayerController.cs
@@ -1,21 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-
-    
+    private int count;
+    public Text countText;
     public float speed;
     public float jump;
     float moveVelocity;
 
-    
+
     bool grounded = true;
+
+    void Start()
+    {
+
+    SetCountText();
+    }
 
     void Update()
     {
-        
+
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.W))
         {
             if (grounded)
@@ -26,7 +33,7 @@ public class PlayerController : MonoBehaviour
 
         moveVelocity = 0;
 
-        
+
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             moveVelocity = -speed;
@@ -39,7 +46,7 @@ public class PlayerController : MonoBehaviour
         GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
 
     }
-    
+
     void OnTriggerEnter2D()
     {
         grounded = true;
@@ -48,5 +55,22 @@ public class PlayerController : MonoBehaviour
     {
         grounded = false;
     }
-    
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+
+
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+
+    }
 }
